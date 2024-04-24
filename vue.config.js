020ff70cc25204779aced716195124d3d302aed5
PlasticTong <72696179+PlasticTong@users.noreply.github.com>
2024-04-24 10:19:30 +0800

    second

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer:{
+    port:8087,
+    open: true,//启动项目打开浏览器
+    hot: true,//热更新
+  }
 })

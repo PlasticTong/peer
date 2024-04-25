51424dd1ea021641cc0ed6c3923405dfd172b1dd
PlasticTong <72696179+PlasticTong@users.noreply.github.com>
2024-04-25 11:09:46 +0800

    third

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,43 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+  <div class="font_all">
+    <div class="tools">
+      ControlPanel
+      <ToolsTable></ToolsTable>
+    </div>
+    <div class="fast">
+      FastRecall
+      <FastRecall></FastRecall>
+    </div> 
   </div>
+  
+  <!-- <FastRecall></FastRecall> -->
+  <div class="font_all">
+    <div class="over">
+      OverView
+      <OverView></OverView>
+    </div>
+    <div class="compare">
+      CompareView
+      <CompareView></CompareView>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToolsTable from './components/ToolsTable.vue';
+import FastRecall from './components/FastRecall.vue';
+import OverView from './components/OverView.vue';
+import CompareView from './components/CompareView.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ToolsTable,
+    FastRecall,
+    OverView,
+    CompareView
   }
 }
 </script>
@@ -25,4 +51,39 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-</style>
+
+.tools {
+  color: #999;
+  /* 定义文字颜色为灰色 */
+  width: 25%;
+  height: 50%;
+}
+.fast {
+  color: #999;
+  margin-left: 10px;
+  /* 定义文字颜色为灰色 */
+  width: 75%;
+  height: 50%;
+}
+
+.font_all {
+  height: 100%;
+  display: flex;
+}
+
+.over{
+  color: #999;
+  margin-right: 5px;
+  /* 定义文字颜色为灰色 */
+  width: 50%;
+  height: 50%;
+}
+
+.compare{
+  color: #999;
+  margin-left: 5px;
+  /* 定义文字颜色为灰色 */
+  width: 50%;
+  height: 50%;
+}
+</style>./components/ToolsTable.vue
---
+++ src/components/OverView.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="h3">
+      总体概览
+    </div>
+
+
+</template>
+
+<script>
+export default {
+  name: 'OverView',
+  props: {
+    msg: String
+  },
+  created() {
+    let test = window.innerWidth;
+    let test2 = window.innerHeight;
+    console.group(test2, test);
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.h3 {
+  /* background-color: red; */
+  /* width: 200px; 定义方框宽度 */
+  /* height: 50%; */
+  background-color: #f0f0f0;
+  /* 定义方框背景颜色 */
+  border-radius: 10px;
+  /* 定义圆角大小 */
+  padding: 10px;
+  /* 定义内边距 */
+}
+
+
+</style>
